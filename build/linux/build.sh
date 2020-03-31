467b1b120f519c0abb788217b18b2f18ffd8f634
alex-dow <adow@psikon.com>
2020-03-30 22:09:36 -0400

    cleanup

--- build/linux/build.sh
+++ build/linux/build.sh
@@ -26,45 +26,46 @@ cd $PROJECT_DIR/psistats
 
 PROJECT_NAME="$( cargo config package.name | cut -d '"' -f 2 )"
 PROJECT_VERSION="$( cargo config package.version | cut -d '"' -f 2 )"
-if [ -z "$BUILD_NUMBER" ]; then DEBIAN_VERSION=$PROJECT_VERSION; else DEBIAN_VERSION="$PROJECT_VERSION-$BUILD_NUMBER"; fi
+if [ -z "$BUILD_NUMBER" ]; then DEBIAN_VERSION=$PROJECT_VERSION; else DEBIAN_VERSION="${PROJECT_VERSION}-${BUILD_NUMBER}"; fi
 
 cd $PROJECT_DIR
 
 echo Project location: $PROJECT_DIR
 echo Project name: $PROJECT_NAME
 echo Project version: $PROJECT_VERSION
+echo Debian version: $DEBIAN_VERSION
 
-RELEASE_DIR=$PROJECT_DIR/target/$TARGET/release
-ARTIFACT_DIR=$RELEASE_DIR/artifacts
-UNZIPPED_DIR=$RELEASE_DIR/unzipped/${PROJECT_NAME}-${DEBIAN_VERSION}-${PROJECT_ARCH}
+RELEASE_DIR=${PROJECT_DIR}/target/$TARGET/release
+ARTIFACT_DIR=${RELEASE_DIR}/artifacts
+UNZIPPED_DIR=${RELEASE_DIR}/unzipped/${PROJECT_NAME}-${DEBIAN_VERSION}-${PROJECT_ARCH}
 
 rm -rf $ARTIFACT_DIR
-rm -rf $RELEASE_DIR/unzipped
+rm -rf ${RELEASE_DIR}/unzipped
 
 cargo build --target $TARGET --release
 
 mkdir -p $UNZIPPED_DIR
 mkdir -p $ARTIFACT_DIR
-mkdir -p $UNZIPPED_DIR/plugins
+mkdir -p ${UNZIPPED_DIR}/plugins
 cp $RELEASE_DIR/psistats $UNZIPPED_DIR
-cp $RELEASE_DIR/libplugin_*.so $UNZIPPED_DIR/plugins
+cp $RELEASE_DIR/libplugin_*.so ${UNZIPPED_DIR}/plugins
 cp $PROJECT_DIR/LICENSE $UNZIPPED_DIR
 cp $PROJECT_DIR/psistats.toml $UNZIPPED_DIR
 
 cd $RELEASE_DIR/unzipped
 
-tar -cvzf ${ARTIFACT_DIR}/${PROJECT_NAME}-${DEBIAN_VERSION}-${PROJECT_ARCH}.tar.gz *
+tar -cvzf ${ARTIFACT_DIR}/${PROJECT_NAME}_${DEBIAN_VERSION}_${PROJECT_ARCH}.tar.gz *
 
 cd $PROJECT_DIR
 
-DEBIAN_DIR=$RELEASE_DIR/debian
+DEBIAN_DIR=${RELEASE_DIR}/debian
 mkdir -p $DEBIAN_DIR
 
-cargo deb --deb-version $DEBIAN_VERSION --target $TARGET -p psistats
+cargo deb --deb-version=${DEBIAN_VERSION} --target ${TARGET} -p psistats
 
-DEBIAN_TARGET_DIR=$PROJECT_DIR/target/$TARGET/debian
-DEBIAN_FILE=$DEBIAN_TARGET_DIR/${PROJECT_NAME}_${DEBIAN_VERSION}_${target_map[$TARGET]}.deb
+DEBIAN_TARGET_DIR=${PROJECT_DIR}/target/${TARGET}/debian
+DEBIAN_FILE=${DEBIAN_TARGET_DIR}/${PROJECT_NAME}_${DEBIAN_VERSION}_${target_map[$TARGET]}.deb
 
 cp $DEBIAN_FILE $ARTIFACT_DIR
-mkdir -p $PROJECT_DIR/target/release/artifacts
-cp -r $ARTIFACT_DIR/* $PROJECT_DIR/target/release/artifacts
+mkdir -p ${PROJECT_DIR}/target/release/artifacts
+cp -r ${ARTIFACT_DIR}/* ${PROJECT_DIR}/target/release/artifacts
